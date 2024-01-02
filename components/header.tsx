1a62b92a2f6c83fa49b970e1dc3a3c8d6b4abcd2
Jana-r-dh-an-an <janardhanan.rs475@gmail.com>
2024-01-02 18:05:15 +0530

    commited all

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,9 @@ import {motion} from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
 
+
+
+
 export default function Header(){
 
     return <header className='z-[999] relative'>
@@ -13,12 +16,17 @@ export default function Header(){
         backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
         initial={{y:-100, x:"-50%", opacity:0}}
         animate={{y:0,x:"-50%", opacity:1}}>
+
+</motion.div>
+
+
             <nav className='flex fixed top-[0.15rem] left-1/2 h-12
             -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
                 
                 <ul className='flex
                 w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial]
-                sm:flex-nowrap sm:gap-5'>{
+                sm:flex-nowrap sm:gap-5'>
+                    {
                  links.map((link) =>(
                     <li className='flex w-full items-center justify-center px-3 py-3
                     hover:text-gray-950' key={link.hash}>
@@ -32,7 +40,7 @@ export default function Header(){
 
             </nav>
 
-        </motion.div>
+       
 
     </header>
 }
